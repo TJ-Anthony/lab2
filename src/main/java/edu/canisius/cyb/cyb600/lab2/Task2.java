c845e1aa6dafb59e0f6f843834a186b7af5bb18d
Anthony <amendrysajr@gmail.com>
2024-03-10 19:53:46 -0400

    Task 2 module 2 unfinshed

--- src/main/java/edu/canisius/cyb/cyb600/lab2/Task2.java
+++ src/main/java/edu/canisius/cyb/cyb600/lab2/Task2.java
@@ -3,14 +3,39 @@ package edu.canisius.cyb.cyb600.lab2;
 public class Task2 {
 
     public String method1(String parameter1){
-        return parameter1;
+        if (parameter1 == null) {
+            return "";
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        for (int i = parameter1.length() - 1; i >= 0; i--) {
+            reversed.append(Character.toUpperCase(parameter1.charAt(i)));
+        }
+        return reversed.toString();
     }
 
     public String method2(String parameter2){
-        return parameter2;
+        if (parameter2 == null || parameter2.isEmpty()){
+            return "";
+        }
+
+        char firstChar = parameter2.charAt(0);
+        StringBuilder repeated = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            repeated.append(firstChar);
+        }
+        return repeated.append(parameter2.substring(1)).toString();
     }
 
     public String method3(String parameter3){
-        return parameter3;
+        if (parameter3 == null) {
+            return "";
+        }
+        String[] words = parameter3.split(" ");
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i>= 0; i--) {
+            reversed.append(words[i]).append(" ");
+        }
+        return reversed.toString().trim();
     }
 }
